016c4885109bf5317d8f74b1ccda868b865a155c
Klein Kiuvu Gachuhi <klein.gachuhi@student.moringaschool.com>
2025-07-30 15:47:07 +0300

    Arithmetic

--- class.cpp
+++ class.cpp
@@ -1,9 +1,25 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int age;
+//     cout<< "Enter your age: ";
+//     cin >> age;
+//     cout << "You are " << age << " old." <<"\n";
+// }
+
 #include <iostream>
 using namespace std;
 
-int main() {
-    int age;
-    cout<< "Enter your age: ";
-    cin >> age;
-    cout << "You are " << age << "old." <<"\n";
+int main(){
+    int avg;
+   cout << "Enter average: ";
+   cin >>avg;
+    
+    if (avg >=40) {
+        cout<< "Congratulations you have passed!";
+    } else {
+        cout<< "You have failed!";
+    }
+
 }
