2b28f21dad0260816e62f993095bae4760c03449
Klein Kiuvu Gachuhi <klein.gachuhi@student.moringaschool.com>
2025-07-31 12:52:11 +0300

    Checking the oldest and the youngest

--- Code-along/age.cpp
+++ Code-along/age.cpp
@@ -1,19 +1,54 @@
-#include <iostream>
+// #include <iostream>
 
+// using namespace std;
+
+// int main() {
+//     int age;
+//     cout << "Enter your age :";
+//     cin >> age;
+
+//     if (age>=18 && age <=90) {
+//         cout<< "Congratulations you are eligible to vote!" << "\n";
+//     } else if (age > 90)
+//     {
+//         cout<<"You are dead!!" << "\n";
+//     }else
+//      {
+//         cout <<"Sorry, you are under 18 not allowed to vote!" "\n";
+//     }
+// }
+
+
+# include <iostream>
 using namespace std;
 
 int main() {
-    int age;
-    cout << "Enter your age :";
-    cin >> age;
+    int age1;
+    int age2;
+    int age3;
 
-    if (age>=18 && age <=90) {
-        cout<< "Congratulations you are eligible to vote!" << "\n";
-    } else if (age > 90)
-    {
-        cout<<"You are dead!!" << "\n";
-    }else
-     {
-        cout <<"Sorry, you are under 18 not allowed to vote!" "\n";
+    cout << "Enter age1 :";
+    cin >> age1;
+    cout << "Enter age2 : ";
+    cin >>age2;
+    cout << "Enter age3 :";
+    cin >> age3;
+
+    if (age1 > age2 && age1 > age3) {
+        cout << "Age1 is the oldest" << "\n";
+
+    } else if (age2 > age1 && age2 > age3) {
+        cout << "Age2 is the oldest" << "\n";
+    } else if (age3 > age1 && age3 > age2) {
+        cout << "Age3 is the oldest" << "\n";
+    }
+
+    if (age1 < age2 && age1 < age3) {
+        cout << "Age1 is the youngest" << "\n";
+
+    } else if (age2 < age1 && age2 < age3) {
+        cout << "Age2 is the youngest" << "\n";
+    } else if (age3 < age1 && age3 < age2) {
+        cout << "Age3 is the youngest" << "\n";
     }
 }
--- Code-along/integer.cpp
+++ Code-along/integer.cpp
@@ -1,20 +1,38 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int number;
+//     cout << "Enter a number :";
+//     cin >>number;
+
+//     if (number > 0) {
+//         cout<< "You entered a positive number" << number << "\n";
+
+//     } else if (number < 0)
+//     {
+//         cout<< "You entered a negative number " <<number << "\n";
+//     }
+//     else {
+//         cout<< "You entered 0";
+//     }
+    
+// }
+
+
+
 #include <iostream>
 using namespace std;
 
 int main() {
     int number;
+
     cout << "Enter a number :";
     cin >>number;
 
-    if (number > 0) {
-        cout<< "You entered a positive number" << number << "\n";
-
-    } else if (number < 0)
-    {
-        cout<< "You entered a negative number " <<number << "\n";
+    if (number > 10 && number < 20) {
+        cout << "The number entered is between ten and twenty";
+    } else {
+        cout << "The number entered is not between ten and twenty";
     }
-    else {
-        cout<< "You entered 0";
-    }
-    
 }
